a0a0c91d2c9e61f4e06e079a8e33cca5f84bfd55
pratikaherMBT <pratikaher98@gmail.com>
2025-05-29 01:29:40 +0530

    database url updated

--- config.py
+++ config.py
@@ -5,5 +5,12 @@ import os
 class Config:
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:1234@localhost/news_db'
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@host.docker.internal/news_db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@host.docker.internal/news_db'
+
+    # SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:d:JA9hNPoR5ZH97lCnh)GoSRg<G<@news-website-postgres.cv4ocsocwxy0.ap-south-1.rds.amazonaws.com:5432/postgres'
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:.3%5DGi1.oF3%23SaQD1K%28jns3bnsqb8@news-website-postgres.cv4ocsocwxy0.ap-south-1.rds.amazonaws.com:5432/postgres"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
